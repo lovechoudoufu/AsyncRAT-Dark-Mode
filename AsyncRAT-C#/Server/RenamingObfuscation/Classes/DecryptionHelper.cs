0c50ca80396517fa1dfaad4ef48ff9f869894201
Dollhouse0672 <gigabyte5967@outlook.com>
2025-05-01 16:52:52 +0400

    DecryptionHelper.cs

---
+++ AsyncRAT-C#/Server/RenamingObfuscation/Classes/DecryptionHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.RenamingObfuscation.Classes
+{
+    internal static class DecryptionHelper
+    {
+        public static string Decrypt_Base64(string dataEnc)
+        {
+            try
+            {
+                return Encoding.UTF8.GetString(Convert.FromBase64String(dataEnc));
+            }
+
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
