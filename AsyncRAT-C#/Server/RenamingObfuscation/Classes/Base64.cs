dbf5e224d01812901446ab3a82aee39ad98087bc
Dollhouse0672 <gigabyte5967@outlook.com>
2025-05-01 16:52:48 +0400

    Base64.cs

---
+++ AsyncRAT-C#/Server/RenamingObfuscation/Classes/Base64.cs
@@ -0,0 +1,25 @@
+﻿using Server.RenamingObfuscation.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.RenamingObfuscation.Classes
+{
+    public class Base64 : ICrypto
+    {
+        public string Encrypt(string dataPlain)
+        {
+            try
+            {
+                return Convert.ToBase64String(Encoding.UTF8.GetBytes(dataPlain));
+            }
+
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
