2bdece8465726965a8a299e7a3b7a7852b448b8a
Dollhouse0672 <gigabyte5967@outlook.com>
2025-05-01 16:52:35 +0400

    AssemblyInfo.cs

---
+++ AsyncRAT-C#/Server/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("AsyncRAT DarkMode")]
+[assembly: AssemblyDescription("by unknown")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AsyncRAT Dark Mode")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.5.8.0")]
+[assembly: AssemblyFileVersion("0.5.8.0")]
